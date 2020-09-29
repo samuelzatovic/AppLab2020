6c905e3d1778eb31b209deb73732eb94f720cf46
samuelzatovic <szatovic@gmail.com>
2020-09-29 08:23:19 +0200

    Snail cvičenie

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,11 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020!");
+        double steps = 100;
+        double length = 0.4;
+        double height = 0.2;
+        double totaldistance = (steps/height) * (length + height);
+        System.out.println(totaldistance);
     }
+
 }
