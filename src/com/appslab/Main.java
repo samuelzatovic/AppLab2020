f57cc430c50d9b60f06a901266590d8a84383ed1
samuelzatovic <szatovic@gmail.com>
2020-10-08 09:20:27 +0200

    RPS dorobit

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,7 +3,15 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Dokončiť ;)");
+        System.out.println(RPS("rock", "paper"));
+    }
+    public static String RPS(String player1, String player2)
+    {
+        if (player1.equals(player2))
+            return "TIE";
+        if (player1.equals("rock") && player2.equals("paper"))
+            return "PLAYER 2 WINS";
+        return "";
 
     }
 }
