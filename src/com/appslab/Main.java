7b4282bef1b5c3339d5d5cf888816af712cbea04
samuel zaťovič <szatovic@gmail.com>
2020-10-15 12:19:07 +0200

    RPS Done

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,7 +3,7 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(RPS("rock", "paper"));
+        System.out.println(RPS("paper", "scissors"));
     }
     public static String RPS(String player1, String player2)
     {
@@ -11,7 +11,16 @@ public class Main {
             return "TIE";
         if (player1.equals("rock") && player2.equals("paper"))
             return "PLAYER 2 WINS";
-        return "";
-
+        if (player1.equals("rock") && player2.equals("scissors"))
+            return "PLAYER 1 WINS";
+        if (player1.equals("paper") && player2.equals("rock"))
+            return "PLAYER 1 WINS";
+        if (player1.equals("paper") && player2.equals("scissors"))
+            return "PLAYER 2 WINS";
+        if (player1.equals("scissors") && player2.equals("rock"))
+            return "PLAYER 2 WINS";
+        if (player1.equals("scissors") && player2.equals("paper"))
+            return "PLAYER 1 WINS";
+        return "INVALID INPUT";
     }
 }
