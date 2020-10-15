092e9f755ef25e0f62632c63863dac61cef2569b
samuel zaťovič <szatovic@gmail.com>
2020-10-15 15:13:34 +0200

    DifferenceMaxMin done.

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -1,8 +1,18 @@
 package com.appslab;
 
+import java.util.Arrays;
+import java.util.DuplicateFormatFlagsException;
+
 public class Main {
 
-    public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020!");
+    public static void main(String[] args)
+    {
+        int[] array = {44, 32, 86, 19};
+        System.out.println(DifferenceMaxMin(array));
+    }
+    public static int DifferenceMaxMin(int[] array)
+    {
+        Arrays.sort(array);
+        return array[array.length - 1] - array[0];
     }
 }
