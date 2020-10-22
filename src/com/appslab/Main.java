6d2fe1830a05a219710e0eb16519f802b6111c13
samuel zaťovič <szatovic@gmail.com>
2020-10-22 11:26:51 +0200

    Can Capture

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,10 +3,14 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(checkEnding("ahoj", "oj"));
+        System.out.println(canCapture("H4", "H3"));
     }
-    public static boolean checkEnding(String a, String b)
-    {
-        return (a.endsWith(b));
+
+    public static boolean canCapture(String a, String b) {
+
+        {
+            return a.charAt(0) == b.charAt(0) || a.charAt(1) == b.charAt(1);
+        }
     }
 }
+
