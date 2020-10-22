9ef4bca0bdf8a5663488dcb05b4653e865a9c20f
samuel zaťovič <szatovic@gmail.com>
2020-10-22 11:14:28 +0200

    WarOfNumbers

--- src/com/appslab/Main.java
+++ src/com/appslab/Main.java
@@ -3,6 +3,22 @@ package com.appslab;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello AppsLab 2020!");
+        int[] numbers = {1, 4, 2, 3, 19, 28, 142, 333};
+        System.out.println(warOfNumbers(numbers));
+    }
+    public static boolean isNumOdd(int num){
+        return num % 2 != 0;
+    }
+    public static int warOfNumbers(int[] numbers)
+    {
+        int even = 0, odd = 0;
+        for (int number : numbers) {
+            if (isNumOdd(number)) {
+                odd += number;
+            } else {
+                even += number;
+            }
+        }
+        return Math.abs(even - odd);
     }
 }
